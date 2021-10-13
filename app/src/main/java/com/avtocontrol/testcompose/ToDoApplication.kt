eed1bda9c2cf707a88980479d8925cee2b110364
Maksim Spirkin <dev@avtocontrol.com>
2021-10-13 03:34:18 +0300

    feat: create ToDoApplication * add ToDoApplication is AndroidManifest * create DatabaseModule * add @HiltAndroidApp * add @AndroidEntryPoint

---
+++ app/src/main/java/com/avtocontrol/testcompose/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package com.avtocontrol.testcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication : Application()
---
+++ app/src/main/java/com/avtocontrol/testcompose/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.avtocontrol.testcompose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.avtocontrol.testcompose.data.dao.ToDoDatabase
+import com.avtocontrol.testcompose.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+
+    @Singleton
+    @Provides
+    fun provideDatabase (
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
